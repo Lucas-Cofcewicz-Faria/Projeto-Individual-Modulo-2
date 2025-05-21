41e6160af5d52e0c0c049702ed62a2dbbd2424fd
Lucas Cofcewicz Faria <lucas.faria@sou.inteli.edu.br>
2025-05-21 11:59:35 -0300

    Bug: Routes magicamente se consertaram e uma pequena mudança na tabela

--- testEnv.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-console.log({
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_USER: process.env.DB_USER,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_NAME: process.env.DB_NAME,
-});
