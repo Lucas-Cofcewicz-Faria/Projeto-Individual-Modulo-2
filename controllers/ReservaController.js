5427ae00acdfde21baf88fdfbee3bc1bf1816526
Lucas Cofcewicz Faria <lucas.faria@sou.inteli.edu.br>
2025-05-27 10:59:19 -0300

    Update ReservaController.js

--- controllers/ReservaController.js
+++ controllers/ReservaController.js
@@ -1,11 +1,12 @@
 // controllers/TarefaController.js
-const pool = require('../config/database');
+const pool = require("../config/database");
 
 // Criar uma nova reserva
 exports.criarReserva = async (req, res) => {
   const { user_id, sala_id, status, data, horario, preco_id } = req.body;
 
-  const query = 'INSERT INTO reservas (user_id, sala_id, status, data, horario, preco_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
+  const query =
+    "INSERT INTO reservas (user_id, sala_id, status, data, horario, preco_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *";
   const values = [user_id, sala_id, status, data, horario, preco_id];
 
   try {
@@ -19,7 +20,7 @@ exports.criarReserva = async (req, res) => {
 
 // Listar todas as reservas
 exports.listarReservas = async (req, res) => {
-  const query = 'SELECT * FROM reservas';
+  const query = "SELECT * FROM reservas";
 
   try {
     const result = await pool.query(query);
@@ -42,7 +43,7 @@ exports.editarReserva = async (req, res) => {
   try {
     const result = await pool.query(query, values);
     if (result.rows.length === 0) {
-      return res.status(404).json({ message: 'Reserva não encontrada' });
+      return res.status(404).json({ message: "Reserva não encontrada" });
     }
     res.status(200).json(result.rows[0]);
   } catch (err) {
@@ -53,15 +54,15 @@ exports.editarReserva = async (req, res) => {
 // Excluir uma reserva
 exports.excluirReserva = async (req, res) => {
   const { idreserva } = req.params;
-  const query = 'DELETE FROM reservas WHERE idreserva = $1 RETURNING *';
+  const query = "DELETE FROM reservas WHERE idreserva = $1 RETURNING *";
   const values = [idreserva];
 
   try {
     const result = await pool.query(query, values);
     if (result.rows.length === 0) {
-      return res.status(404).json({ message: 'Reserva não encontrada' });
+      return res.status(404).json({ message: "Reserva não encontrada" });
     }
-    res.status(200).json({ message: 'Reserva excluída com sucesso' });
+    res.status(200).json({ message: "Reserva excluída com sucesso" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
